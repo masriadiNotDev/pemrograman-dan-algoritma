581df775340cc125e1a0ff5ae78282a56287f7a2
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:50:11 +0800

    Update bubble_sort.py

--- final test/4_Algoritma_Bubble_Sorting_Selection_Sorting_dan_Insertion_Sorting/bubble_sort.py
+++ final test/4_Algoritma_Bubble_Sorting_Selection_Sorting_dan_Insertion_Sorting/bubble_sort.py
@@ -1,1 +1,13 @@
 
+# masriadi sisfo b D0425309
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+data = [50, 20, 40, 10, 30]
+bubble_sort(data)
+print("Bubble Sort:", data)

3a2b7faaff1e56e834873cefe7a49bd68cac7051
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:50:52 +0800

    Update selection_sort.py

--- final test/4_Algoritma_Bubble_Sorting_Selection_Sorting_dan_Insertion_Sorting/selection_sort.py
+++ final test/4_Algoritma_Bubble_Sorting_Selection_Sorting_dan_Insertion_Sorting/selection_sort.py
@@ -1,1 +1,16 @@
+# masriadi sisfo b D0425309
 
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+data = [50, 20, 40, 10, 30]
+selection_sort(data)
+print("Selection Sort:", data)

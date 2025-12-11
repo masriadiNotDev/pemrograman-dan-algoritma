35812425a62a2cb150ba1edb5f6c7c43cc43fdfe
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:48:11 +0800

    Update 3 dimensi.py

--- final test/1_Tipe_Data_array_1d_2d_3d/3 dimensi.py
+++ final test/1_Tipe_Data_array_1d_2d_3d/3 dimensi.py
@@ -1,1 +1,18 @@
 
+# masriadi sisfo b D0425309
+
+# Contoh array 3 dimensi (blok data)
+array_3d = [
+    [
+        [1, 2], 
+        [3, 4]
+    ],
+    [
+        [5, 6], 
+        [7, 8]
+    ]
+]
+
+print("Array 3 Dimensi:")
+for blok in array_3d:
+    print(blok)

273797c9887a4bce77a973d411407c4fd04ca47e
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:48:38 +0800

    Implement basic stack operations in single stack.py
    Added basic stack implementation with push and pop functions.

--- final test/2_Algoritma_Stack_Single_dan_Double/single stack.py
+++ final test/2_Algoritma_Stack_Single_dan_Double/single stack.py
@@ -1,1 +1,21 @@
 
+# masriadi sisfo b D0425309
+
+stack = []
+
+def push(item):
+    stack.append(item)
+
+def pop():
+    if len(stack) == 0:
+        return "Stack kosong"
+    return stack.pop()
+
+# Contoh penggunaan
+push(10)
+push(20)
+push(30)
+
+print("Isi Stack:", stack)
+print("Pop:", pop())
+print("Isi Stack setelah Pop:", stack)

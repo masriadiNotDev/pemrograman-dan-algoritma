a19b474783504a47c383f1979113ce6ae1be53e5
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:51:23 +0800

    Update 5_Dictionary.py

--- final test/5_Dictionary.py
+++ final test/5_Dictionary.py
@@ -1,1 +1,11 @@
 
+# masriadi sisfo b D0425309
+
+mahasiswa = {
+    "nama": "Masriadi",
+    "nim": "D0425309",
+    "prodi": "Sistem Informasi"
+}
+
+for key, value in mahasiswa.items():
+    print(f"{key}: {value}")

0edf4784af4479ca041e428aec127f9b932552e6
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:49:38 +0800

    Update binary_search.py

--- final test/3_Algoritma_Binary_Search_dan_Interpolation_Search/binary_search.py
+++ final test/3_Algoritma_Binary_Search_dan_Interpolation_Search/binary_search.py
@@ -1,1 +1,20 @@
+# masriadi sisfo b D0425309
 
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+data = [10, 20, 30, 40, 50]
+print("Cari 30:", binary_search(data, 30))

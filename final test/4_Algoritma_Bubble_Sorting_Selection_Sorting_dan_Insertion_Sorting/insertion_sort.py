581316eb44b41c02e167fd94b268f75601ee835e
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:50:34 +0800

    Update insertion_sort.py

--- final test/4_Algoritma_Bubble_Sorting_Selection_Sorting_dan_Insertion_Sorting/insertion_sort.py
+++ final test/4_Algoritma_Bubble_Sorting_Selection_Sorting_dan_Insertion_Sorting/insertion_sort.py
@@ -1,1 +1,17 @@
+# masriadi sisfo b D0425309
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+        arr[j + 1] = key
+
+data = [50, 20, 40, 10, 30]
+insertion_sort(data)
+print("Insertion Sort:", data)
 

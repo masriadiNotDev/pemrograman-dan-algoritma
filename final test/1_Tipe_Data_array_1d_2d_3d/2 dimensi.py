1d0884e174bfd501888d57520070df55fd9a7a56
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:47:54 +0800

    Update 2 dimensi.py

--- final test/1_Tipe_Data_array_1d_2d_3d/2 dimensi.py
+++ final test/1_Tipe_Data_array_1d_2d_3d/2 dimensi.py
@@ -1,1 +1,13 @@
 
+# masriadi sisfo b D0425309
+
+# Contoh array 2 dimensi (matriks)
+array_2d = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print("Array 2 Dimensi:")
+for baris in array_2d:
+    print(baris)

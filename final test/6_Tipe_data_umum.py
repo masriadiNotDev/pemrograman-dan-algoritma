cdb74469937da1e38a464612459e4e51df39fd0e
masriadiNotDev <adiganzxd@gmail.com>
2025-12-11 17:45:22 +0800

    Create 6_Tipe_data_umum.py

---
+++ final test/6_Tipe_data_umum.py
@@ -0,0 +1,5 @@
+# tipe data umum
+produk = "asus" # string 
+harga = 12.000 # float atau bilangan desimal
+jumlah = 122 # integer atau nilai angka
+apakahTersedia = True # tipe data boolean yg hnya berisi kondisi true (benar) dan false (salah)
